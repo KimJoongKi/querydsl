10bd036a92450d68c6972658069fbd3f0c953be1
KimJoongKi <thoug12@naver.com>
2021-02-12 16:12:23 +0900

    조인 - 페치 조인

--- src/test/java/yaddoong/querydsl/QuerydslBasicTest.java
+++ src/test/java/yaddoong/querydsl/QuerydslBasicTest.java
@@ -15,6 +15,8 @@ import yaddoong.querydsl.entity.QTeam;
 import yaddoong.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.as;
@@ -297,6 +299,41 @@ public class QuerydslBasicTest {
 //                .extracting("username")
 //                .containsExactly("teamA", "teamB");
 
+    }
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+
+    @Test
+    public void fetchJoinNo() {
+
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("패치 조인 미적용").isFalse();
+    }
+
+    @Test
+    public void fetchJoinUse() {
+
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("패치 조인 미적용").isTrue();
     }
 
 
