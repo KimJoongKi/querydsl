777860a3c4b07f9db69f8fdeaa376d41617ee612
KimJoongKi <thoug12@naver.com>
2021-02-14 16:30:07 +0900

    순수 JPA 리포지토리와 Querydsl

--- src/main/java/yaddoong/querydsl/QuerydslApplication.java
+++ src/main/java/yaddoong/querydsl/QuerydslApplication.java
@@ -1,7 +1,11 @@
 package yaddoong.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
