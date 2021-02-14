f5109ba6c25c2a33ed7fff30066c2d3892d73410
KimJoongKi <thoug12@naver.com>
2021-02-15 06:28:17 +0900

    조회 API 컨트롤러 개발

---
+++ src/main/java/yaddoong/querydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package yaddoong.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import yaddoong.querydsl.dto.MemberSearchCondition;
+import yaddoong.querydsl.dto.MemberTeamDto;
+import yaddoong.querydsl.respository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
